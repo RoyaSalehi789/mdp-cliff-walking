d072849ea83aaa2ac8c3d51c855a4706e638fdcc
nimiico <nimaeslami206@gmail.com>
2023-12-12 05:35:13 +0330

    Final Edit with new enviroment

--- mdp/main.py
+++ mdp/main.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import gymnasium as gym
 from gymnasium.envs.toy_text.cliffwalking import CliffWalkingEnv
@@ -10,7 +11,6 @@ RIGHT = 1
 DOWN = 2
 LEFT = 3
 image_path = path.join(path.dirname(gym.__file__), "envs", "toy_text")
-
 
 class CliffWalking(CliffWalkingEnv):
     def __init__(self, is_hardmode=True, num_cliffs=10, *args, **kwargs):
@@ -29,9 +29,9 @@ class CliffWalking(CliffWalkingEnv):
                 new_col = np.random.randint(0, 11)
                 state = (new_row, new_col)
                 if (
-                        (state not in self.cliff_positions)
-                        and (state != self.start_state)
-                        and (state != self.terminal_state)
+                    (state not in self.cliff_positions)
+                    and (state != self.start_state)
+                    and (state != self.terminal_state)
                 ):
                     self._cliff[new_row, new_col] = True
                     if not self.is_valid():
@@ -187,77 +187,205 @@ class CliffWalking(CliffWalkingEnv):
             )
 
 
-def update_values(env, policy, gamma, threshold=0.0001):
-    values = [0 for i in range(env.observation_space.n)]
-    q_values = [[0 for i in range(4)] for j in range(env.observation_space.n)]
+# Create an environment
+def main():
+    env = CliffWalking(render_mode="human")
+    observation, info = env.reset(seed=30)
+    enviroment = enviroment_init()
+    values, policy = mdp_algorithm(enviroment, env)
+    # Define the maximum number of iterations
+    max_iter_number = 1000
+    next_state = 36
+    counter = 0
+    fail = 0
+    for i in range(len(policy)):
+        print(i, ':', policy[i], end='   ')
+    print('\n\n')
+    for i in range(len(policy)):
+        print(i, ':', values[i], end='   ')
 
-    while True:
+    for __ in range(max_iter_number):
+        # TODO: Implement the agent policy here
+        # Note: .sample() is used to sample random action from the environment's action space
+
+        # Choose an action (Replace this random action with your agent's policy)
+        action = policy[next_state]
+
+        # Perform the action and receive feedback from the environment
+        next_state, reward, done, truncated, info = env.step(action)
+
+        if next_state == 47:
+            counter += 1
+
+        if reward == -100:
+            fail += 1
+
+        # print(next_state)
+
+        if done or truncated:
+            observation, info = env.reset()
+
+    # Close the environment
+    print(counter)
+    print(fail)
+
+    env.close()
+
+def actions(s, a):
+    r = s // 12
+    c = s % 12
+
+    action = []
+    if a == 0:
+        action.append(0)
+        if c != 0:
+            action.append(3)
+        else:
+            action.append(-1)
+
+        if c != 11:
+            action.append(1)
+        else:
+            action.append(-1)
+
+    if a == 1:
+        action.append(1)
+        if r != 0:
+            action.append(0)
+        else:
+            action.append(-1)
+
+        if r != 3:
+            action.append(2)
+        else:
+            action.append(-1)
+
+    if a == 2:
+        action.append(2)
+        if c != 0:
+            action.append(3)
+        else:
+            action.append(-1)
+
+        if c != 11:
+            action.append(1)
+        else:
+            action.append(-1)
+
+    if a == 3:
+        action.append(3)
+        if r != 0:
+            action.append(0)
+        else:
+            action.append(-1)
+
+        if r != 3:
+            action.append(2)
+        else:
+            action.append(-1)
+
+    return action
+
+
+def possible_actions(s):
+    r = s // 12
+    c = s % 12
+    action = []
+
+    if r != 0:
+        action.append(0)
+    if r != 3:
+        action.append(2)
+    if c != 0:
+        action.append(3)
+    if c != 11:
+        action.append(1)
+
+    return action
+
+def next_state(s, a):
+    if a == 0:
+        return s - 12
+    if a == 1:
+        return s + 1
+    if a == 2:
+        return s + 12
+    if a == 3:
+        return s - 1
+    if a == -1:
+        return s
+
+
+def update_values(enviroment, env, policy, gamma, threshold=0.0001):
+    values = [0 for i in range(env.observation_space.n)]
+    q_values = {}
+    for state in range(48):
+        q_values[state] = {}
+        for ac in possible_actions(state):
+            q_values[state][ac] = 0
+
+    cliff = [u[0] * 12 + u[1] for u in env.cliff_positions]
+    t = 0
+    while (t < 1000):
         distance = 0
+        last_val = values.copy()
         for state in range(env.observation_space.n):
-            for action in range(4):
-                sum = 0
-                prev_value = q_values[state][action]
-                for probability, next_state, reward, done in env.P[state][action]:
-                    if (next_state == 47):
-                        reward = 1000
-                    sum += probability * values[next_state]
+            if (state == 47):
+                mx_q = 4000
+            elif (state in cliff):
+                mx_q = -100
+            else:
+                for action in possible_actions(state):
+                    q_values[state][action] = 0
+                    sum = 0
+                    for probability, reward, nxt_state in enviroment[state][action]:
+                        q_values[state][action] += probability * (reward + gamma * values[nxt_state])
+                    mx_q = q_values[state][policy[state]]
 
-                q_values[state][action] = reward + (gamma * sum)
-                distance = max(distance, abs(prev_value - q_values[state][action]))
-            values[state] = q_values[state][policy[state]]
-        if distance < threshold:
-            break
-    return q_values
+            values[state] = mx_q
+
+        t += 1
+
+    return q_values, values
+
+def enviroment_init():
+    enviroment = {}
+    for state in range(47):
+        c = state % 12
+        enviroment[state] = {}
+        for action in possible_actions(state):
+            enviroment[state][action] = []
+            h = actions(state, action)
+            for i in range(len(h)):
+                reward = (-1 / (10 * (c + 1))) ** 3
+                enviroment[state][action].append((1 / 3, reward, next_state(state, h[i])))
+
+    return enviroment
 
 
-def mdp_algorithm(env, gamma=0.9):
+def mdp_algorithm(enviroment, env, gamma=0.9):
     values = [0 for i in range(env.observation_space.n)]
-    policy = [0 for i in range(env.observation_space.n)]
-
-    while True:
-        q_values = update_values(env, policy, gamma)
+    policy = [possible_actions(i)[0] for i in range(48)]
+    t = 1
+    while (t < 100):
+        q_values, values = update_values(enviroment, env, policy, gamma)
         changed = False
 
         for state in range(env.observation_space.n):
-            mxId = 0
-            for action in range(4):
+            mxId = possible_actions(state)[0]
+            for action in possible_actions(state):
                 if q_values[state][action] > q_values[state][mxId]:
                     mxId = action
             if policy[state] != mxId:
                 policy[state] = mxId
                 changed = True
-        if changed == False:
-            return values, policy
+
+        if not changed:
+            break
+
+        t += 1
+    return values, policy
 
 
-# Create an environment
-env = CliffWalking(render_mode="human")
-observation, info = env.reset(seed=30)
-values, policy = mdp_algorithm(env)
-# Define the maximum number of iterations
-max_iter_number = 1000
-for i in range(len(policy)):
-    print(i, ':', policy[i])
-print(env.P)
-next_state = 36
-counter = 0
-for __ in range(max_iter_number):
-    # TODO: Implement the agent policy here
-    # Note: .sample() is used to sample random action from the environment's action space
-
-    # Choose an action (Replace this random action with your agent's policy)
-    action = policy[next_state]
-    if next_state == 47:
-        counter += 1
-    # Perform the action and receive feedback from the environment
-    next_state, reward, done, truncated, info = env.step(action)
-
-    # print(next_state)
-
-    if done or truncated:
-        observation, info = env.reset()
-
-# Close the environment
-
-env.close()
-print(counter)
+if __name__ == '__main__':
+    main()
